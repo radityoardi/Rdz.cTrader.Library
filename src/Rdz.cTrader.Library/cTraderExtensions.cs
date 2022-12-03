56d4e1408fe882ae270bedc72cd8bdfc186d398d
Radityo Ardi <radityo.ardi@live.com>
2022-12-03 15:14:50 +0800

    removing redundant functions aside from Rdz.Net.Library

--- src/Rdz.cTrader.Library/cTraderExtensions.cs
+++ src/Rdz.cTrader.Library/cTraderExtensions.cs
@@ -93,11 +93,6 @@ namespace Rdz.cTrader.Library
 		}
 
 
-		public static bool IsNotEmpty(this string input)
-		{
-			return input != null && !string.IsNullOrEmpty(input) && !string.IsNullOrWhiteSpace(input);
-		}
-
 		public static DateTime ParseDateTime(this string input, string DateFormat)
 		{
 			DateTime dt;
@@ -146,89 +141,6 @@ namespace Rdz.cTrader.Library
 				sDigit = string.Concat("0.", new String('0', digit));
 			}
 			return sDigit;
-		}
-
-		public static bool IsPositive(this double input)
-		{
-			return (!double.IsNaN(input) && input > 0);
-		}
-		public static bool IsPositive(this int input)
-		{
-			return (input > 0);
-		}
-		public static bool IsNegative(this double input)
-		{
-			return (!double.IsNaN(input) && input < 0);
-		}
-		public static bool IsNegative(this int input)
-		{
-			return (input < 0);
-		}
-		public static bool IsZero(this double input)
-		{
-			return (!double.IsNaN(input) && input == 0);
-		}
-		public static bool IsAbove(this double input, double comparer)
-		{
-			return input > comparer;
-		}
-		public static bool IsAboveOrEqual(this double input, double comparer)
-		{
-			return input >= comparer;
-		}
-		public static bool IsBelow(this double input, double comparer)
-		{
-			return input < comparer;
-		}
-		public static bool IsBelowOrEqual(this double input, double comparer)
-		{
-			return input <= comparer;
-		}
-		public static bool IsBetween(this double input, double comparerA, double comparerB)
-		{
-			if (comparerA >= comparerB)
-			{
-				return input.IsBelowOrEqual(comparerA) && input.IsAboveOrEqual(comparerB);
-			}
-			else if (comparerA <= comparerB)
-			{
-				return input.IsBelowOrEqual(comparerB) && input.IsAboveOrEqual(comparerA);
-			}
-			else
-				return input == comparerA && input == comparerB;
-		}
-		public static bool IsAbove(this int input, int comparer)
-		{
-			return input > comparer;
-		}
-		public static bool IsAboveOrEqual(this int input, int comparer)
-		{
-			return input >= comparer;
-		}
-		public static bool IsBelow(this int input, int comparer)
-		{
-			return input < comparer;
-		}
-		public static bool IsBelowOrEqual(this int input, int comparer)
-		{
-			return input <= comparer;
-		}
-		public static bool IsBetween(this int input, int comparerA, int comparerB)
-		{
-			if (comparerA >= comparerB)
-			{
-				return input.IsBelowOrEqual(comparerA) && input.IsAboveOrEqual(comparerB);
-			}
-			else if (comparerA <= comparerB)
-			{
-				return input.IsBelowOrEqual(comparerB) && input.IsAboveOrEqual(comparerA);
-			}
-			else
-				return input == comparerA && input == comparerB;
-		}
-		public static bool IsZero(this int input)
-		{
-			return (input == 0);
 		}
 		public static double CutHalf(this double input, int digits = 5)
 		{
